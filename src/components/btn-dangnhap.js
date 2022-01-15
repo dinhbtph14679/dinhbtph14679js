4af23ccd12a24ade43ed8e02e108aee529132e64
dinhbtph14679 <dinhbtph14679@fpt.edu.vn>
2022-01-15 11:28:10 +0700

    update sign in and sign up

---
+++ src/components/btn-dangnhap.js
@@ -0,0 +1,9 @@
+const BTNDangNhap = {
+    render() {
+        return /* html */`
+            <a href="/singin">Đăng nhập</a>
+            <a href="/singup">Đăng ký</a>
+        `;
+    },
+};
+export default BTNDangNhap;
